faad8948836fe82f3fb6141934cb66d6b45eda08
tmax818 <tylermaxwell661@gmail.com>
2020-02-13 21:45:38 -0800

    init commit

---
+++ app.js
@@ -0,0 +1,3 @@
+let message = "Hi honey, I love you. Happy Valentine's Day. I am sorry I didn't get you a card. I built you a website instead. I hope you like it. Love, Jake"
+
+document.querySelector(".btn").addEventListener('click', () => alert(message))
